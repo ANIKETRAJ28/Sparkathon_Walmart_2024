c5fc59ec72475144727cc1d031880bb56f8673da
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-08-15 19:34:55 +0530

    Optimal Price generated

--- src/components/CompareProduct.jsx
+++ src/components/CompareProduct.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 import Amazon from "../assets/Amazon.png"
+import axios from "axios";
 
-export default function Product({title, price, id, deleteProduct}) {
+export default function Product({title, price, productId, deleteProduct}) {
+    const [optimalPrice, setOptimalPrice]= useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleGeneratePrice = async () => {
+        setLoading(true); // Start loading
+        try {
+            const response = await axios.get(`http://localhost:3001/api/v1/product/${productId}/generatePrice`);
+            setOptimalPrice(response.data.optimalPrice);
+        } catch (error) {
+            console.error('Failed to fetch optimal price', error);
+        } finally {
+            setLoading(false); // Stop loading
+        }
+    };
+    
     return (
-        <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="shadow-xl card bg-base-300 w-96">
             <figure>
                 <img
                 src={Amazon}
@@ -11,10 +28,12 @@ export default function Product({title, price, id, deleteProduct}) {
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <div>Current Price: {price}</div>
-                <div>Optimal Price</div>
-                <div className="w-full flex justify-between">
-                    <button className="btn btn-primary w-fit">Generate Your Price</button>
-                    <button onClick={() => deleteProduct(id)} className="btn btn-primary w-fit">Delete</button>
+                <div>Optimal Price: {optimalPrice ? `${optimalPrice}` : 'Not generated yet'} </div>
+                <div className="flex justify-between w-full">
+                    <button onClick={handleGeneratePrice} disabled={loading} className="btn btn-primary w-fit">
+                        {loading ? 'Loading...' : 'Generate Your Price'}
+                    </button>
+                    <button onClick={() => deleteProduct(productId)} className="btn btn-primary w-fit">Delete</button>
                 </div>
             </div>
         </div>
