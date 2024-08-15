f109149c0bc8a34df2c3357d11a5fd5552c26851
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-08-15 12:18:51 +0530

    fixed bug

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -4,14 +4,13 @@ import Company from "../pages/Company";
 import Product from "../pages/Product";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import ProtectedRoute from "../components/ProtectedRoute";
 
 export default function MainRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/competitors" element={<ProtectedRoute><Company/></ProtectedRoute>}/>
-            <Route path="/product" element={<ProtectedRoute><Product/></ProtectedRoute>}/>
+            <Route path="/competitors" element={<Company/>}/>
+            <Route path="/product" element={<Product/>}/>
             <Route path="/signup" element={<Signup/>}/>
             <Route path="/login" element={<Login/>}/>
         </Routes>  
